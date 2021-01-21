872be841ffaedbbee0fb6ac2a42198567ec059a3
Fangfei Yang <33738100+FangfeiY@users.noreply.github.com>
2021-01-20 22:35:06 -0500

    Added "completed" column to todo_item table.

--- TodoItem.py
+++ TodoItem.py
@@ -4,6 +4,7 @@ class TodoItem(db.Model):
     __tablename__ = 'todo_item'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(),nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
