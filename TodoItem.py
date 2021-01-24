983557a17df00f836d655cec897167837f3e942d
Fangfei Yang <33738100+FangfeiY@users.noreply.github.com>
2021-01-24 14:27:17 -0500

    Added TodoList model. Updated TodoItem to associate with TodoList. Upgraded db schema.

--- TodoItem.py
+++ TodoItem.py
@@ -1,10 +1,12 @@
 from TodoDb import db
+from TodoList import TodoList
 
 class TodoItem(db.Model):
     __tablename__ = 'todo_item'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(),nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todo_list.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
---
+++ TodoList.py
@@ -0,0 +1,10 @@
+from TodoDb import db
+
+class TodoList(db.Model):
+    __tablename__ = 'todo_list'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(),nullable=False)
+    todos = db.relationship('TodoItem', backref='parent_list', lazy=True)
+
+    def __repr__(self):
+        return f'<Todo list {self.id} {self.name}>'
