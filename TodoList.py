3661b3a74f8823c62767fb07aa6087b06977cd2f
Fangfei Yang <33738100+FangfeiY@users.noreply.github.com>
2021-01-31 12:26:35 -0500

    Added completed column to todo_list model. Upgraded DB schema using DB migration file.

--- TodoList.py
+++ TodoList.py
@@ -4,6 +4,7 @@ class TodoList(db.Model):
     __tablename__ = 'todo_list'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(),nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     todos = db.relationship('TodoItem', backref='parent_list', lazy=True)
 
     def __repr__(self):
